a2cac906e0e9adf8f49b19e321f2d5cf3af49040
Melissa Cabral <melissacabral@gmail.com>
2022-02-22 10:39:22 -0800

    update parse-logout to remove the access token from the DB on logout

--- includes/parse-logout.php
+++ includes/parse-logout.php
@@ -1,7 +1,29 @@
 <?php 
 //url will look like login.php?action=logout
 if( isset( $_GET['action'] ) AND $_GET['action'] == 'logout' ){
-	//TODO: remove the access_token from the DB
+	//remove the access token from the DB
+	$user_id = 0;
+	if(isset($logged_in_user['user_id'])){
+		$user_id = $logged_in_user['user_id'];
+	}
+	$access_token = '';
+	if(isset($_COOKIE['access_token'])){
+		$access_token = $_COOKIE['access_token'];
+	}elseif(isset($_SESSION['access_token'])){
+		$access_token = $_SESSION['access_token'];
+	}
+	
+	//Nullify the access token from this user's DB row
+   	$result = $DB->prepare( 'UPDATE users 
+							SET
+							access_token = NULL
+							WHERE user_id = :id OR access_token = :token
+							LIMIT 1' );
+	$result->execute( array(
+						'token' => $access_token,
+						'id' => $user_id,
+					) );
+	
 
 	//invalidate all cookies and session vars
 	setcookie('access_token', 0, time() - 9999);
